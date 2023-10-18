069cd3af0225e396edbf2b400680d20ea037446c
diana-buts <73740149+diana-buts@users.noreply.github.com>
2023-10-18 17:24:12 +0300

    Update tasks.js

--- js-tasks/tasks.js
+++ js-tasks/tasks.js
@@ -17,7 +17,7 @@ function makeNegative(num) {
 //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
 function move(position, roll) {
-  return position + roll2;
+  return position + roll*2;
 }
 
 //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
