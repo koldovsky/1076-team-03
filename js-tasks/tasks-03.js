fd0b3c00f65b7b25209a024490dc68626357378a
diana-buts <73740149+diana-buts@users.noreply.github.com>
2023-10-23 16:58:35 +0300

    Create tasks-03.js

---
+++ js-tasks/tasks-03.js
@@ -0,0 +1,27 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  const radius = circle.radius;
+  const result = 2 * Math.PI * radius;
+  return parseFloat(result.toFixed(6));
+}
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const giveMeFiveArray = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      giveMeFiveArray.push(key);
+    }
+    if (obj[key].length === 5) {
+      giveMeFiveArray.push(obj[key]);
+    }
+  }
+  return giveMeFiveArray;
+}
