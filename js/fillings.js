9868ce4745e9c33f36bac2b51614174c6a17e885
diana-buts <73740149+diana-buts@users.noreply.github.com>
2023-10-25 11:40:20 +0000

    Add js, edit HTML and CSS files

--- js/fillings.js
+++ js/fillings.js
@@ -1,4 +1,6 @@
 document.addEventListener('partialsLoaded', () => {
     import('./form-title.js');   
-    import('./burger.js');    
+    import('./burger.js');
+    import('./cake-fillings.js');
+    import('./latest-fillings.js')
 });
